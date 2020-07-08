c2ddfc0ddfdfef2781101c84e130892edaac3c69
wangchao <ydwang_net@163.com>
2020-07-08 11:20:11 +0800

    有效括号

--- algorithm/stack/有效括号_test.go
+++ algorithm/stack/有效括号_test.go
@@ -7,14 +7,22 @@ import (
 )
 
 func TestValidBrackets(t *testing.T) {
-	var str = "())("
-	res := CheckBrackets(str)
-	fmt.Printf("有效性：%v\n", res)
+	var str = "()()"
+	res := CheckBracketsArrayStack(str)
+	fmt.Printf("数组栈方式验证有效性：%v\n", res)
+	result := CheckBracketsListStack(str)
+	fmt.Printf("链表栈方式验证有效性：%v\n", result)
 }
 
-func CheckBrackets(str string) bool {
+func CheckBracketsArrayStack(str string) bool {
 	//使用数组栈实现
 	res := ArrayStack(str)
+	return res
+}
+
+func CheckBracketsListStack(str string) bool {
+	//使用链表栈实现
+	res := ListStack(str)
 	return res
 }
 
@@ -22,9 +30,66 @@ type Node struct {
 	Val  string
 	Next *Node
 }
+type List struct {
+	Size int
+	Head *Node
+}
 
 func ListStack(str string) bool {
+	var listStack = new(List) //链表栈
+	listStack.Size = 0
+	listStack.Head = new(Node)
+	strArr := strings.Split(str, "")
+	for _, v := range strArr {
+		if (v == ")" || v == "}" || v == "]") && listStack.Size == 0 {
+			return false
+		}
+		if v == "(" || v == "{" || v == "[" {
+			//入栈
+			fmt.Printf("%v入栈\n", v)
+			PushToStack(listStack, v)
+		} else {
+			//取栈内最后一个node值与v匹配，匹配成功则继续，匹配失败直接 return false
+			stackVal := PopFormStack(listStack)
+			fmt.Printf("出栈一个值%v与%v匹配\n", stackVal, v)
+			if !(stackVal == "{" && v == "}") && !(stackVal == "(" && v == ")") && !(stackVal == "[" && v == "]") {
+				return false
+				break
+			}
+		}
+	}
+	if listStack.Size == 0 {
+		return true
+	}
+	return false
+}
 
+//入栈
+func PushToStack(l *List, str string) bool {
+	var n = new(Node)
+	n.Val = str
+	n.Next = nil
+	ListSize := l.Size
+	var now = l.Head //头结点
+	for i := 0; i < ListSize; i++ {
+		now = now.Next
+	}
+	now.Next = n
+	l.Size += 1
+	return true
+}
+
+//出栈并返回尾节点值
+func PopFormStack(l *List) string {
+	listSize := l.Size
+	var pointer = l.Head
+	for i := 0; i < listSize-1; i++ {
+		pointer = pointer.Next
+	}
+	val := pointer.Next.Val
+	pointer.Next = nil
+	l.Size -= 1
+	return val
 }
 
 //数组栈
